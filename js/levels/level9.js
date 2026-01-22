14ea1bb8e951c2f902a29e496e1e69848b83ad2a
arjunsharma1511 <96876989+arjunsharma1511@users.noreply.github.com>
2026-01-22 23:11:30 +0530

    Update level9.js

--- js/levels/level9.js
+++ js/levels/level9.js
@@ -1,6 +1,6 @@
-eval(atob("Y29uc3QgYW5zID0gIm9iZnVzY2F0ZWQi"));
+eval("window." + atob("YW5zID0gIm9iZnVzY2F0ZWQi"));
 
 document.getElementById("challenge").innerText =
   "What was just created?";
 
-window.level9Check = (input) => input === ans;
+window.level9Check = (input) => input === window.ans;
